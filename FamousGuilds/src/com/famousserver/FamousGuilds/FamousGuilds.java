6fff1630a1a0e09a3cf7a615f432bc5dae41e987
Benjamin Wingert <benjamin.wingert@yahoo.de>
2012-03-16 18:37:00 +0100

    sasf

---
+++ FamousGuilds/src/com/famousserver/FamousGuilds/FamousGuilds.java
@@ -0,0 +1,15 @@
+package com.famousserver.FamousGuilds;
+
+import org.spout.api.plugin.CommonPlugin;
+
+public class FamousGuilds extends CommonPlugin {
+
+	public void onDisable() {
+		
+	}
+
+	public void onEnable() {
+		
+	}
+
+}
