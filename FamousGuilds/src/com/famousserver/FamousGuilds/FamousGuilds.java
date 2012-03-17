d83d1c3a64c08b05693c65e25ce763b61f297a9a
Benjamin Wingert <benjamin.wingert@yahoo.de>
2012-03-17 14:20:46 +0100

    added default config

--- FamousGuilds/src/com/famousserver/FamousGuilds/FamousGuilds.java
+++ FamousGuilds/src/com/famousserver/FamousGuilds/FamousGuilds.java
@@ -1,10 +1,16 @@
 package com.famousserver.FamousGuilds;
 
+import java.io.File;
+
+import com.famousserver.FamousGuilds.util.Config;
+
 import org.spout.api.plugin.CommonPlugin;
 
 public class FamousGuilds extends CommonPlugin 
 {
 	public static FamousGuilds instance;
+	private Config config;
+	private File configFile = new File("plugins/FamousGuilds/config.yml");
 
 	public void onDisable() 
 	{
@@ -14,6 +20,21 @@ public class FamousGuilds extends CommonPlugin
 	public void onEnable()
 	{
 		instance = this;
+		config = new Config(configFile);
 	}
 
+	public Config getConfig()
+	{
+		return config;
+	}
+	
+	public void saveConfig()
+	{
+		config.save();
+	}
+	
+	public void reloadConfig()
+	{
+		config.reload();
+	}
 }
